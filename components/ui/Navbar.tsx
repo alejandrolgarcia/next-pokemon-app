b4e1c0a1c6299bdb48f0a4cd286fbfd2210b1e40
Luis Garcia <lalejandro.1988@gmail.com>
2022-03-12 14:57:03 -0600

    detalle pokemon, getStaticPaths

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,4 +1,5 @@
-import { Spacer, Text, useTheme } from '@nextui-org/react';
+import { Link, Spacer, Text, useTheme } from '@nextui-org/react';
+import NextLink from 'next/link';
 import Image from 'next/image';
 
 export const Navbar = () => {
@@ -12,22 +13,36 @@ export const Navbar = () => {
         flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'start',
-        padding: '0px 40px 0px 20px',
+        padding: '0 40px 0px 20px',
         backgroundColor: theme?.colors.gray900.value
     }}>
-        <Image
-            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"
-            alt="Icono de la app"
-            width={ 70 }
-            height={ 70 }
-        />
 
-        <Text color='white' h2>P</Text>
-        <Text color='white' h3>okémon</Text>
+        <NextLink href="/" passHref>
+            <Link>
+                <Image
+                    src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"
+                    alt="Icono de la app"
+                    width={ 70 }
+                    height={ 70 }
+                />
+            </Link>
+        </NextLink>
+
+        <NextLink href="/" passHref>
+            <Link>
+                <Text color='white' h2>P</Text>
+                <Text color='white' h3>okémon</Text>
+            </Link>
+        </NextLink>
 
         <Spacer css={{ flex: 1}} />
 
-        <Text color='white' >Favoritos</Text>
+        <NextLink href="/favorites" passHref>
+            <Link>
+                <Text color='white' >Favoritos</Text>
+            </Link>
+        </NextLink>
+
     </div>
   )
 }
